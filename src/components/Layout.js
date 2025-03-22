c2caa0af3fe0fdf3f1f6392d879a1abd6a2c3b47
rasoulnoroui <rasoulnorouzi@live.com>
2025-03-22 01:42:56 +0100

    fullpage support!

---
+++ src/components/Layout.js
@@ -0,0 +1,9 @@
+// ...existing code...
+const Layout = ({ children }) => {
+  return (
+    <div className="layout-container" style={{ width: '100%', maxWidth: '100%' }}>
+      {children}
+    </div>
+  );
+};
+// ...existing code...
